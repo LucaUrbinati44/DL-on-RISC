6c3f6251a47f9fbe2d014161eaa22b1fb686bd00
LucaUrbinati44 <luca.urbinati.44@gmail.com>
2025-08-12 14:41:15 +0200

    Add tflm pio project

---
+++ mcu/esp32-s2-saola-tflm/lib/dequantize_output.h
@@ -0,0 +1,6 @@
+#ifndef TENSORFLOW_LITE_MICRO_EXAMPLES_ML_ON_RISC_DEQUANTIZE_OUTPUT_H_
+#define TENSORFLOW_LITE_MICRO_EXAMPLES_ML_ON_RISC_DEQUANTIZE_OUTPUT_H_
+
+void dequantize_output(int8_t* buffer_in, float scale, int zero_point, float* buffer_out);
+
+#endif  // TENSORFLOW_LITE_MICRO_EXAMPLES_ML_ON_RISC_DEQUANTIZE_OUTPUT_H_
